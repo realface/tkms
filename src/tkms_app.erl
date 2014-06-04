5466d9abcca090e47215d1baa093d3b0830841f8
jason_he <jason_he@me.com>
2014-06-04 16:15:44 +0800

    rebar create-app appid=tkms (version 2.3)

---
+++ src/tkms_app.erl
@@ -0,0 +1,16 @@
+-module(tkms_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    tkms_sup:start_link().
+
+stop(_State) ->
+    ok.
