96a248c4939679d9001f697bac9c154943d48767
jason_he <jason_he@me.com>
2014-07-22 21:48:25 +0800

    try out simple eunit test

--- src/tkms_app.erl
+++ src/tkms_app.erl
@@ -5,12 +5,25 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+%% 测试代码
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    io:format("hello jason"),
     tkms_sup:start_link().
 
 stop(_State) ->
     ok.
+
+%% 测试代码
+-ifdef(TEST).
+simple_test() ->
+    ok = application:start(tkms),
+    ?assertNot(undefined == whereis(tkms_sup)).
+-endif.
